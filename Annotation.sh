75b1cd3bc7de0f90f0592ef20b93e49235b9463c
Tina04021997 <60873217+Tina04021997@users.noreply.github.com>
2021-03-08 17:16:48 +0800

    Update Annotation.sh

--- Annotation.sh
+++ Annotation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author:Tina Yang
-# Date: Mar 6,2021
+# Date: Mar 8,2021
 # By using Ensembl VEP to determine the effect of the variants
 # According to Ensembl, the VEP Cache version should be the same as your Ensembl VEP installation
 # VEP v101
@@ -9,8 +9,8 @@
 REF_PATH="/LVM_data/tina/0220_variant_calling/mm10"
 DATA_PATH="/LVM_data/tina/0220_variant_calling"
 
-#Running VEP
-vep -i $DATA_PATH/FilteredVariant.vcf.gz \
+#Running VEP for SNP
+vep -i $DATA_PATH/PassedVariant_SNP.vcf.gz \
     --cache \
     --cache_version 101 \
     --dir_cache $DATA_PATH/.vep \
@@ -19,4 +19,19 @@ vep -i $DATA_PATH/FilteredVariant.vcf.gz \
     --fork 8 \
     --offline \
     --fasta  $REF_PATH/GRCm38.primary_assembly.genome.fa \
-    -o $DATA_PATH/VEPannotated.vcf
+    --vcf \
+    -o $DATA_PATH/VEPannotatedSNP.vcf
+
+
+#Running VEP for INDEL
+vep -i $DATA_PATH/PassedVariant_indel.vcf.gz \
+    --cache \
+    --cache_version 101 \
+    --dir_cache $DATA_PATH/.vep \
+    --species mus_musculus \
+    --everything \
+    --fork 8 \
+    --offline \
+    --fasta  $REF_PATH/GRCm38.primary_assembly.genome.fa \
+    --vcf \
+    -o $DATA_PATH/VEPannotatedINDEL.vcf
