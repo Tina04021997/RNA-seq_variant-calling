331b7f181447fad6fc6019874c2c222cc84bfc97
Ting (Tina) Yang <60873217+Tina04021997@users.noreply.github.com>
2025-08-20 20:37:03 -0700

    Create ascat.r

---
+++ ascat.r
@@ -0,0 +1,37 @@
+## PrepareHTS and Running ASCAT for all samples ##
+
+args <- commandArgs(trailingOnly = TRUE)
+sample <- args[1]
+gender <- args[2]
+
+library(ASCAT)
+
+# # PrepareHTS: Extracting logR and BAF from HTS data (bam files)
+ascat.prepareHTS(
+  tumourseqfile = paste0("PATH//",sample,"_tumor.bam"),
+  normalseqfile = paste0("/PATH/",sample,"_normal.bam"),
+  tumourname = paste0(sample,"_tumor"),
+  normalname = paste0(sample,"_normal"),
+  allelecounter_exe = "/PATH/bin/alleleCounter",
+  alleles.prefix = "/PATH/hg38/Alleles/G1000_alleles_hg38_chr",
+  loci.prefix = "/PATH/hg38/Loci/G1000_loci_hg38_chr",
+  gender = paste0(gender),
+  genomeVersion = "hg38")
+
+# Running ASCAT
+# For HTS data (WGS, WES and targeted sequencing), gamma must be set to 1 in ascat.runASCAT
+ascat.bc = ascat.loadData(Tumor_LogR_file = paste0(sample,"_tumor_tumourLogR.txt"), 
+                          Tumor_BAF_file = paste0(sample,"_tumor_tumourBAF.txt"), 
+                          Germline_LogR_file = paste0(sample,"_tumor_normalLogR.txt"), 
+                          Germline_BAF_file = paste0(sample,"_tumor_normalBAF.txt"), 
+                          gender = paste0(gender), 
+                          genomeVersion = "hg38")
+
+ascat.plotRawData(ascat.bc, img.prefix = "Before_correction_")
+ascat.bc = ascat.correctLogR(ascat.bc, GCcontentfile = "/PATH/hg38/GC_Correction/GC_G1000_hg38.txt", replictimingfile = "/PATH/hg38/RT_Correction/RT_G1000_hg38.txt")
+ascat.plotRawData(ascat.bc, img.prefix = "After_correction_")
+ascat.bc = ascat.aspcf(ascat.bc, out.dir = NA) # penalty=25 for targeted sequencing data
+ascat.plotSegmentedData(ascat.bc)
+ascat.output = ascat.runAscat(ascat.bc, gamma = 1, write_segments = T) # gamma=1 for HTS data
+QC = ascat.metrics(ascat.bc,ascat.output)
+save(ascat.bc,ascat.output, QC, file= 'ASCAT_objects.Rdata')
